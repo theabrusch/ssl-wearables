c73a534591feaf6a9d4b749b12b4b05fc4465354
Thea Brusch <s164238@student.dtu.dk>
2023-03-24 11:48:39 +0100

    job script

--- job.sh
+++ job.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#BSUB -q gpuv100
+#BSUB -q gpua100
 #BSUB -gpu "num=1:mode=exclusive_process"
 #BSUB -J Capture24_finetuning
 ### number of core
@@ -10,11 +10,7 @@
 #BSUB -R "rusage[mem=128GB]"
 ### Number of hours needed
 #BSUB -N 
-<<<<<<< HEAD
-#BSUB -W 01:00
-=======
-#BSUB -W 12:00
->>>>>>> 49a7722 (job script)
+#BSUB -W 04:00
 ### added outputs and errors to files
 #BSUB -o logs/Output_Capture_24_finetuning_%J.out
 #BSUB -e logs/Error_Capture_24_finetuning_%J.err
@@ -22,8 +18,6 @@
 module load python3/3.7.14
 source ssl-env/bin/activate
 
-<<<<<<< HEAD
-python3 downstream_task_evaluation.py evaluation.num_epoch=200 data=capture24_10s data.data_root=/work3/theb/timeseries/capture24_100hz_w10_o0 evaluation=all
-=======
-python3 downstream_task_evaluation.py evaluation.num_epoch=200 data=capture24_10s data.data_root=/work3/theb/timeseries/capture24_100hz_w10_o0 evaluation=all num_split=5 evaluation.flip_net_path="/zhome/89/a/117273/Desktop/ssl-wearables/model_check_point/mtl_best.mdl" gpu=0 is_verbose=True evaluation.learning_rate=1e-4
->>>>>>> 49a7722 (job script)
+python3 downstream_task_evaluation.py evaluation.num_epoch=200 data=capture24_10s data.data_root=/work3/theb/timeseries/capture24_100hz_w10_o0 evaluation=all output_path=/zhome/89/a/117273/Desktop/ssl-wearables/outputs/ num_split=5 evaluation.flip_net_path="/zhome/89/a/117273/Desktop/ssl-wearables/model_check_point/mtl_best.mdl" gpu=-1 is_verbose=True evaluation.learning_rate=1e-4
+
+
